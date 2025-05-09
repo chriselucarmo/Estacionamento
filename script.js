85b01533f9fa15d03d8b158e4ac56076b4950d80
chriselucarmo <168378987+chriselucarmo@users.noreply.github.com>
2025-05-09 20:42:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 class Estacionamento {
     constructor(valor) {
         this.valor = valor;
-    }
+       
+      }
 
     calcularTempo() {
         if (this.valor < 1.0) {
-            return { mensagem: "Valor insuficiente", troco: 0, tempo: 0 };
+          return { mensagem: "Valor insuficiente", troco: 0, tempo: 0 };
         }
 
-        let tempo = 0;
+         let tempo = 0;
+        let troco = 0;
 
         if (this.valor >= 1.0 && this.valor < 2.0) {
-            tempo = 30; 
+            tempo = 30;
+          troco = this.valor - 1.00;
         } else if (this.valor >= 2.0 && this.valor < 3.0) {
-            tempo = 60; 
+        tempo = 60;
+          troco = this.valor - 2.00;
         } else if (this.valor >= 3.0) {
-            tempo = 120; 
+             tempo = 120;
+          troco = this.valor - 3.00;
         }
-
-        const troco = this.valor -3.0; 
-
-        return { mensagem: `Você tem ${tempo} minutos`, troco: troco.toFixed(2), tempo };
         
-    }
+  
+
+ return { mensagem: `Você tem ${tempo} minutos`, troco: troco.toFixed(2), tempo };
+ 
+}
 }
 
 
